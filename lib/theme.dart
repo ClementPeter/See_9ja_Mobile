f1e9777703ab5c93301324f4b386786f624c19d9
ClementPeter <61213263+ClementPeter@users.noreply.github.com>
2022-11-14 11:45:53 +0100

    See 9ja App

--- lib/theme.dart
+++ lib/theme.dart
@@ -10,29 +10,14 @@ ThemeData theme() {
     appBarTheme: appBarTheme(),
     textTheme: textTheme(),
            colorScheme: ColorScheme.fromSwatch(
-          accentColor: lightGreen, // but now it should be declared like this
+          accentColor: lightGreen, 
         ),
-    //indicatorColor: kPrimaryColor,
-    //inputDecorationTheme: inputDecorationTheme(),
+ 
     visualDensity: VisualDensity.adaptivePlatformDensity,
-    //colorScheme: ColorScheme.fromSwatch().copyWith(secondary: kPrimaryColor),
-   // textSelectionTheme: const TextSelectionThemeData(cursorColor: kPrimaryColor),
+    
   );
 }
 
-// InputDecorationTheme inputDecorationTheme() {
-//   OutlineInputBorder outlineInputBorder = OutlineInputBorder(
-//     borderRadius: BorderRadius.circular(30),
-//    // borderSide: const BorderSide(color: kTextColor),
-//     gapPadding: 10,
-//   );
-//   return InputDecorationTheme(
-//     contentPadding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-//     enabledBorder: outlineInputBorder,
-//     focusedBorder: outlineInputBorder,
-//     border: outlineInputBorder,
-//   );
-// }
 
 TextTheme textTheme() {
   return const TextTheme(
@@ -55,23 +40,3 @@ AppBarTheme appBarTheme() {
     // ).headline6,
   );
 }
-
-
-// InputDecorationTheme inputDecorationTheme() {
-//   OutlineInputBorder outlineInputBorder = OutlineInputBorder(
-//     borderRadius: BorderRadius.circular(28),
-//     borderSide: BorderSide(color: kTextColor),
-//     gapPadding: 10,
-//   );
-//   return InputDecorationTheme(
-//     // If  you are using latest version of flutter then lable text and hint text shown like this
-//     // if you r using flutter less then 1.20.* then maybe this is not working properly
-//     // if we are define our floatingLabelBehavior in our theme then it's not applayed
-//     floatingLabelBehavior: FloatingLabelBehavior.always,
-//     contentPadding: EdgeInsets.symmetric(horizontal: 42, vertical: 20),
-//     enabledBorder: outlineInputBorder,
-//     focusedBorder: outlineInputBorder,
-//     border: outlineInputBorder,
-//   );
-// }
-
